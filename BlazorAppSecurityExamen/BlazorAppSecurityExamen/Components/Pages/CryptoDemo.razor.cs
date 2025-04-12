02913b433c68f4d9b191191348ec444f3b70482b
Mattias <mattias.gijsels@hotmail.com>
2025-04-12 19:29:23 +0200

    trying to get it to work

---
+++ BlazorAppSecurityExamen/BlazorAppSecurityExamen/Components/Pages/CryptoDemo.razor.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+using BlazorAppSecurityExamen.Services;
+
+namespace BlazorAppSecurityExamen.Components.Pages
+{
+    public class CryptoDemoBase : ComponentBase
+    {
+        protected string AESKey = "";
+        protected string PlainText = "";
+        protected string IVKey = "";
+        protected string CipherText = "";
+        protected string DecryptedText = "";
+
+        protected void GenerateKey()
+        {
+            Console.WriteLine("test");
+            AESKey = InfoSec.GenerateKey();
+            IVKey = CipherText = DecryptedText = "";
+        }
+
+        protected void EncryptData()
+        {
+            if (!string.IsNullOrWhiteSpace(AESKey) && !string.IsNullOrWhiteSpace(PlainText))
+            {
+                CipherText = InfoSec.Encrypt(PlainText, AESKey, out string iv);
+                IVKey = iv;
+                DecryptedText = "";
+            }
+        }
+
+        protected void DecryptData()
+        {
+            if (!string.IsNullOrWhiteSpace(CipherText) && !string.IsNullOrWhiteSpace(AESKey) && !string.IsNullOrWhiteSpace(IVKey))
+            {
+                DecryptedText = InfoSec.Decrypt(CipherText, AESKey, IVKey);
+            }
+        }
+    }
+}
